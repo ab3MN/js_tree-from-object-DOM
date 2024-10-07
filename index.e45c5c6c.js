4a54772d740f71171a23a93c004d8b3a967be2d7
ab3MN <ab3mn3@gmail.com>
2024-10-07 15:43:45 +0300

    make build

---
+++ index.e45c5c6c.js
@@ -0,0 +1,2 @@
+!function e(n,t){var r=document.createElement("ul");Object.keys(t).forEach(function(n){var a=document.createElement("li");a.textContent=n,Object.keys(t[n]).length>0&&e(a,t[n]),r.append(a)}),n.append(r)}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.e45c5c6c.js.map
